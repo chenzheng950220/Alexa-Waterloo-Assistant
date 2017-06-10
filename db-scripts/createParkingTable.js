05ac1482385e887165f77be92734228e71d7275b
Zheng Chen <zheng.chen@uwaterloo.ca>
2017-06-10 16:33:42 -0400

    update database script

---
+++ db-scripts/createParkingTable.js
@@ -0,0 +1,41 @@
+/***************************************
+This script creates a table on AWS DynamoDB
+server. Modify the config variable to suit
+your need.
+***************************************/
+
+var AWS = require("aws-sdk");
+AWS.config.update({
+  region: "us-east-1",
+  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var create_params = {
+    TableName : "UWParking",
+    KeySchema: [       
+        { AttributeName: "name", KeyType: "HASH"},  //Partition key
+        { AttributeName: "lot_type", KeyType: "RANGE"}
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "name", AttributeType: "S" },
+        { AttributeName: "lot_type", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+function createTable() {
+    dynamodb.createTable(create_params, function(err, data) {
+        if (err) {
+            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+        }
+    });
+}
+
+createTable();
